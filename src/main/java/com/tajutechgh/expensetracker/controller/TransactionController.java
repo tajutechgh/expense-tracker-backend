21844ac54411246b15f7fc7c3ad709b254ef7324
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-09-09 12:26:07 +0200

    added transaction controller

---
+++ src/main/java/com/tajutechgh/expensetracker/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.tajutechgh.expensetracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+
+
+}
