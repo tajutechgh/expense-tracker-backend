916bedbd89f7d4d73690c266ea19598de9166ae9
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-09-09 11:17:17 +0200

    added transaction repository

---
+++ src/main/java/com/tajutechgh/expensetracker/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.tajutechgh.expensetracker.repository;
+
+import com.tajutechgh.expensetracker.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+
+}
